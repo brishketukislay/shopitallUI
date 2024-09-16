01f6da880370991b0d5f2d2eb9777013e8ce1f80
brishketukislay <brishketu.kislay@gmail.com>
2024-09-16 18:48:00 +0100

    image tiles

--- src/app/tiles/tiles.component.ts
+++ src/app/tiles/tiles.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {ListItemService} from '../../services/list-item.service'
 
 @Component({
   selector: 'app-tiles',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './tiles.component.scss'
 })
 export class TilesComponent {
-
+  itemList:any;
+  constructor(private http:HttpClient, private listItem: ListItemService){}
+  ngOnInit(){
+    // this.itemList = this.http.get('https://shopitall.onrender.com/api/items');
+    this.fetchItemList();
+  }
+  fetchItemList(){
+    this.listItem.getListItem().subscribe((data)=>{
+      this.itemList = data;
+    },
+  (err)=>{
+    console.log('error',err);
+  })
+  }
 }
