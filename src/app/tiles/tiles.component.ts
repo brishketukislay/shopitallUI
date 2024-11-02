cfdfdfa4441818eb72ac9350dd7e5a638ef4a538
brishketukislay <brishketu.kislay@gmail.com>
2024-11-02 23:36:01 +0000

    tile update

--- src/app/tiles/tiles.component.ts
+++ src/app/tiles/tiles.component.ts
@@ -1,45 +1,101 @@
-import { Component } from '@angular/core';
-import {ListItemService} from '../../services/list-item.service'
-import {Router} from '@angular/router'
-// import {LoaderComponent} from '../loader/loader.component'
+// import { Component, NgZone } from '@angular/core';
+// import {ListItemService} from '../../services/list-item.service'
+// import {Router} from '@angular/router'
+// // import {LoaderComponent} from '../loader/loader.component'
+
+// @Component({
+//   selector: 'app-tiles',
+//   templateUrl: './tiles.component.html',
+//   styleUrl: './tiles.component.scss'
+// })
+// export class TilesComponent {
+//   itemList:any;
+//   isLoading:boolean = true;
+//   apiFailed:boolean = false;
+//   constructor(private listItem: ListItemService, private router:Router, private zone:NgZone){}
+//   ngOnInit(){
+//     // this.itemList = this.http.get('https://shopitall.onrender.com/api/items');
+//     this.fetchItemList();
+//   }  fetchItemList(){
+//   //   this.listItem.getListItem().subscribe((data)=>{
+//   //     this.isLoading = false;
+//   //     this.itemList = data;
+//   //   },
+//   // (err)=>{
+//   //   // this.isLoading = false;
+//   //   this.apiFailed = true;
+//   //   console.log('error',err);
+//   // })
+//   this.listItem.getListItem().subscribe({
+//     next: (data)=>{
+//             this.isLoading = false;
+//       this.itemList = data;
+//     },
+//     error:(err)=>{
+//         // this.isLoading = false;
+//     this.apiFailed = true;
+//     console.log('error',err);
+//     }
+//   })
+//   }
+//   productDetail(index:number){
+//     this.zone.run(() => { this.router.navigate(['/product-detail'], { skipLocationChange: false }) });
+    
+//     console.log('index',index)
+//   }
+// }
+//************************** */
+import { Component, OnDestroy, NgZone } from '@angular/core';
+import { ListItemService } from '../../services/list-item.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tiles',
   templateUrl: './tiles.component.html',
-  styleUrl: './tiles.component.scss'
+  styleUrls: ['./tiles.component.scss']
 })
-export class TilesComponent {
-  itemList:any;
-  isLoading:boolean = true;
-  apiFailed:boolean = false;
-  constructor(private listItem: ListItemService, private router:Router){}
-  ngOnInit(){
-    // this.itemList = this.http.get('https://shopitall.onrender.com/api/items');
+export class TilesComponent implements OnDestroy {
+  itemList: any;
+  isLoading: boolean = true;
+  apiFailed: boolean = false;
+  private listSubscription!: Subscription;
+
+  constructor(
+    private listItem: ListItemService,
+    private router: Router,
+    private zone: NgZone
+  ) {}
+  ngOnInit() {
     this.fetchItemList();
-  }  fetchItemList(){
-  //   this.listItem.getListItem().subscribe((data)=>{
-  //     this.isLoading = false;
-  //     this.itemList = data;
-  //   },
-  // (err)=>{
-  //   // this.isLoading = false;
-  //   this.apiFailed = true;
-  //   console.log('error',err);
-  // })
-  this.listItem.getListItem().subscribe({
-    next: (data)=>{
-            this.isLoading = false;
-      this.itemList = data;
-    },
-    error:(err)=>{
-        // this.isLoading = false;
-    this.apiFailed = true;
-    console.log('error',err);
-    }
-  })
   }
-  productDetail(index:number){
-    this.router.navigate(['/product-detail']);
-    console.log('index',index)
+
+  fetchItemList() {
+    this.listSubscription = this.listItem.getListItem().subscribe({
+      next: (data) => {
+        this.isLoading = false;
+        this.itemList = data;
+      },
+      error: (err) => {
+        this.apiFailed = true;
+        console.log('error', err);
+      }
+    });
+  }
+
+  productDetail(index: number) {
+    this.zone.run(() => {
+      this.router.navigate(['/product-detail'], {
+        queryParams: { reload: new Date().getTime() },
+        replaceUrl: true
+      });
+    });
+    console.log('index', index);
+  }
+
+  ngOnDestroy() {
+    if (this.listSubscription) {
+      this.listSubscription.unsubscribe();
+    }
   }
 }
